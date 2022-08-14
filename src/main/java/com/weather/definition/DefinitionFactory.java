a0f61154b4e5fc7ca1bed81b9ca9fdf1bf3e2607
김태영 <freeptk1@gmail.com>
2022-08-14 16:48:41 +0900

    feat: array type over two demention
    - ex: List<List<Integer>>

--- src/main/java/com/weather/definition/DefinitionFactory.java
+++ src/main/java/com/weather/definition/DefinitionFactory.java
@@ -4,46 +4,95 @@ import com.weather.JsonType;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public class DefinitionFactory {
 
     public static JsonFieldDefinition from(Class<?> type){
-        List<JsonFieldDefinition> childs = getJsonFieldDefinitions(type);
-        return new ObjectType(null, JsonType.OBJECT, childs);
+        List<JsonFieldDefinition> children = getJsonFieldDefinitions(type);
+        return new ObjectType(null, JsonType.OBJECT, children);
     }
 
     private static JsonFieldDefinition from(String fieldName, Class<?> type){
-        List<JsonFieldDefinition> childs = getJsonFieldDefinitions(type);
-        return new ObjectType(fieldName, JsonType.OBJECT, childs);
+        List<JsonFieldDefinition> children = getJsonFieldDefinitions(type);
+        return new ObjectType(fieldName, JsonType.OBJECT, children);
     }
 
     @NotNull
     private static List<JsonFieldDefinition> getJsonFieldDefinitions(Class<?> type) {
-        List<JsonFieldDefinition> childs = new ArrayList<>();
+        List<JsonFieldDefinition> children = new ArrayList<>();
 
         Field[] declaredFields = type.getDeclaredFields();
         for (Field declaredField : declaredFields) {
-            childs.add(getJsonFieldDefinition(declaredField));
-        } return childs;
+            children.add(getJsonFieldDefinition(declaredField));
+        } return children;
     }
-
-    // Array - 제네릭 타입 지원
 
     private static JsonFieldDefinition getJsonFieldDefinition(Field field){
         Objects.requireNonNull(field);
 
         Class<?> currentType = field.getType();
+        return generateJsonDefinition(field, currentType);
+    }
+
+    private static JsonFieldDefinition generateJsonDefinition(Field field, Class<?> currentType) {
         JsonType jsonType = JsonType.getJsonType(currentType);
+        String fieldName = field == null ? null : field.getName();
         switch(jsonType){
             case OBJECT:
-                return from(field.getName(), currentType);
+                return from(fieldName, currentType);
             case ARRAY:
-                return null;
+                return generateArrayDefinition(field);
             default:
-                return new NormalType(field.getName(), jsonType);
+                return new NormalType(fieldName, jsonType);
         }
     }
+
+    private static JsonFieldDefinition generateArrayDefinition(Field field){
+
+        String[] types = tokenizeGenericType(field);
+        Queue<Class<?>> classes = new LinkedList<>();
+
+        for (int i = 1; i < types.length; i++) {
+            Class<?> genericClass;
+            try {
+                genericClass = Class.forName(types[i]);
+            } catch (ClassNotFoundException e) {
+                throw new IllegalStateException(types[i] + " is invalid class name.", e);
+            }
+            classes.offer(genericClass);
+        }
+        JsonFieldDefinition child = generateArrayDefinitionTree(classes);
+        List<JsonFieldDefinition> children = new ArrayList<>();
+        children.add(child);
+
+        return new ArrayType(field.getName(), JsonType.ARRAY, children);
+    }
+
+    private static JsonFieldDefinition generateArrayDefinitionTree(Queue<Class<?>> classes){
+        if(classes.isEmpty()) return null;
+
+        Class<?> currentType = classes.remove();
+        JsonType jsonType = JsonType.getJsonType(currentType);
+
+        switch(jsonType){
+            case OBJECT:
+                return from(null, currentType);
+            case ARRAY:
+                JsonFieldDefinition child = generateArrayDefinitionTree(classes);
+                List<JsonFieldDefinition> children = new ArrayList<>();
+                children.add(child);
+                return new ArrayType(null, JsonType.ARRAY, children);
+            default:
+                return new NormalType(null, jsonType);
+        }
+
+    }
+
+
+    private static String[] tokenizeGenericType(Field field) {
+        //List<List<Integer>> => List, List, Integer
+        return field.getGenericType().getTypeName().replaceAll(">", "").split("<");
+    }
+
 }
--- src/test/java/com/weather/definition/DefinitionFactoryTest.java
+++ src/test/java/com/weather/definition/DefinitionFactoryTest.java
@@ -1,18 +1,24 @@
 package com.weather.definition;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 
 
 class DefinitionFactoryTest {
 
-    @DisplayName("")
     @Test
     void test1(){
         JsonFieldDefinition root = DefinitionFactory.from(DefinitionTest.class);
         for (JsonFieldDefinition child : root.getChildren()) {
             System.out.println(child);
         }
+
+        assertThat(root.getChildren().size()).isEqualTo(5);
     }
 
     static class DefinitionTest{
@@ -20,6 +26,7 @@ class DefinitionFactoryTest {
         private Integer age;
         private boolean gender;
         private ObjectType object;
+        private List<List<ObjectType>> array;
     }
 
     static class ObjectType{
